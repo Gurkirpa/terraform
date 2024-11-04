1ead421c0046d93ccfeeb9960926dbb79a14904c
Gurkirpa <jasneet3637@gmail.com>
2024-11-05 03:26:54 +0600

    Update main.tf

--- Day1/main.tf
+++ Day1/main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0866a3c8686eaeeba"  
-    instance_type = "t2.micro"
+# Define an EC2 instance
+resource "aws_instance" "Guri_Aman" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t2.micro"
+  availability_zone =  "us-east-1a"
+
+  tags = {
+    Name = "Guri_Aman"
+  }
 }
+
