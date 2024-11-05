4e93547e3e4b64b7b61dad52a4d90440ac5f30f5
Gurkirpa <jasneet3637@gmail.com>
2024-11-05 10:50:07 +0600

    Update main.tf

--- Day2/main.tf
+++ Day2/main.tf
@@ -1,1 +1,11 @@
-#Define main content
+# Define an EC2 instance
+resource "aws_instance" "Guri_Aman" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  availability_zone =  "${var.zone}"
+
+  tags = {
+    Name = "Guri_Aman"
+  }
+}
+
