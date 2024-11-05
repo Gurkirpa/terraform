d301efe4519602b1f620c78be6c5b126dd3a6110
Gurkirpa <jasneet3637@gmail.com>
2024-11-05 11:41:11 +0600

    Update main.tf

--- Day3/main.tf
+++ Day3/main.tf
@@ -1,11 +1,65 @@
-# Define an EC2 instance
-resource "aws_instance" "Guri_Aman" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone =  "${var.zone}"
+##############################
+#       create VPC
+##############################
+
+resource "aws_vpc" "Guru_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "Guri_Aman"
+    Name = "Guru_vpc"
   }
 }
+##############################
+#    Public Subnet
+##############################
+
+resource "aws_subnet" "pub-sub-1" {
+  vpc_id            = aws_vpc.Guru_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.zone}"
+
+  tags = {
+    Name = "pub-sub-1"
+  }
+}
+##############################
+#     Internet Gateway
+##############################
+
+resource "aws_internet_gateway" "Guru_igw" {
+  vpc_id = aws_vpc.Guru_vpc.id
+
+  tags = {
+    Name = "Guru_igw"
+  }
+}
+
+###############################
+#    Route Table
+###############################
+
+resource "aws_route_table" "guru-rt" {
+  vpc_id = aws_vpc.Guru_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Guru_igw.id
+  }
+
+  tags = {
+    Name = "guru-rt"
+  }
+}
+
+############################
+#   RT Assocation
+############################
+
+resource "aws_route_table_association" "guru-rta" {
+  subnet_id      = aws_subnet.pub-sub-1.id
+  route_table_id = aws_route_table.guru-rt.id
+}
+
 
