6ab5dab2efe99420375ec4dca95b75abf2961ad7
Gurkirpa <jasneet3637@gmail.com>
2024-11-05 11:38:43 +0600

    Update output.tf

--- Day3/output.tf
+++ Day3/output.tf
@@ -1,5 +1,27 @@
 #Display output here
-output "public_ip" {
-  description = "Public IP address assigned to the host by EC2"
-  value       = "${aws_instance.Guri_Aman.public_ip}"
+
+
+# Output for the VPC ID
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.Guru_vpc.id
 }
+
+# Output for the Subnet ID
+output "subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.pub-sub-1.id
+}
+
+#Output for Internet Gateway
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.Guru_igw.id
+}
+
+# Output for the Route Table ID
+output "route_table_id" {
+  description = "The ID of the route table associated with the public subnet"
+  value       = aws_route_table.guru-rt.id
+}
+
